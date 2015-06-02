da2f398a9e30c9ee38b9853f59e35b3228ddb301
misharnet <misharnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 06:06:30 +0000

    Uploading widget
    git-svn-id: https://plugins.svn.wordpress.org/ns-author-widget/trunk@1172449 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,56 @@
+﻿=== NS Author Widget ===
+Contributors: Miodrag Rasic
+Tags: author widget, author box, netscripter, wordpress, widget, plugin,
+Requires at least: 3.0
+Tested up to: 4.2.2
+Stable tag: trunk
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+NS Author Widget is a sidebar or footer widget that displays on single posts with Post Author\'s name, avatar, description, link to all posts and social profiles.
+
+== Description ==
+NS Author Widget is a sidebar or footer widget that displays on single posts with Post Author\'s name, avatar, description, link to all posts and social profiles. This plugin adds five extra fields to user\'s profile page (Dashboard/Users > Your Profile), where you can enter your Social Profiles URL.
+If your Email address is registered in gravatar, then the author's gravatar image will be displayed.
+
+== Installation ==
+ - From The WordPress Dashboard
+
+    Navigate to the \'Add New\' in the plugins dashboard
+    Search for \'NS Author Widget\'
+    Click \'Install Now\'
+    Activate the plugin on the Plugin dashboard.
+
+ - Uploading to your WordPress Dashboard
+
+    Navigate to the \'Add New\' in the plugins dashboard
+    Navigate to the \'Upload\' area
+    Select ns-author-widget.zip from your PC or Lap Top
+    Click \'Install Now\'
+    Activate the plugin in the Plugin dashboard.
+
+ - Using FTP Client
+
+    Download ns-author-widget.zip
+    Extract the ns-author-widget directory to your PC or Lap Top
+    Upload directory to the /wp-content/plugins/ directory
+    Activate the plugin in the Plugin dashboard.
+
+ - IMPORTANT:
+    Go to the Dashboard/Users > Your Profile > Social Profiles: enter necessary data.
+
+== Frequently Asked Questions ==
+= Does the plugin support localization? =
+
+Yes, you can find .POT file in a folder \"languages\".
+
+Please send your localization files (.mo и .po) to admin@netscripter.me
+
+== Screenshots ==
+1. Five new added fields in User's profile page.
+2. front end look with social icons.
+3. Front end look without social icons.
+4. Widget Panel.
+
+== Changelog ==
+This is completely brand new version of this plugin.
---
+++ ns-author-widget.php
@@ -0,0 +1,192 @@
+<?php
+/*
+Plugin Name: NS Author Widget
+Plugin URI: http://netscripter.me/ns-author-widget/
+Description: NS Author Widget is a sidebar or footer widget that displays on single posts with Post Author\'s name, avatar, description, link to all posts and social profiles.
+Version: 1.1
+Author: Miodrag Rasic
+Author URI: http://netscripter.me/
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0-standalone.html
+*/
+
+// Additing Action hook widgets_init
+add_action( 'widgets_init', 'ns_aw'); 
+
+function nsaw_CSS() {
+wp_enqueue_style( 'nsaw',plugins_url('css\nsaw.css',__FILE__) );
+}
+add_action( 'wp_enqueue_scripts', 'nsaw_CSS' );
+
+function ns_aw() {
+register_widget( 'ns_aw_info' );
+}
+
+class ns_aw_info extends WP_Widget {
+function ns_aw_info () {
+		$this->WP_Widget('ns_aw_info', 'NS Author Widget','Select the category to display');	}
+
+public function form( $instance ) { 
+ if ( isset( $instance[ 'ns_aw_custom_title' ])) {
+			$ns_aw_custom_title = $instance[ 'ns_aw_custom_title' ];	
+		}
+else {
+		$ns_aw_custom_title = 'Post Author';
+}?>
+		<p>Title: <input class="widefat" name="<?php echo $this->get_field_name( 'ns_aw_custom_title' ); ?>" type="text" value="<?php echo esc_attr( $ns_aw_custom_title );?>" /></p>
+		
+<?php }
+
+function update($new_instance, $old_instance) {
+$instance = $old_instance;
+$instance['ns_aw_custom_title'] = ( ! empty( $new_instance['ns_aw_custom_title'] ) ) ? strip_tags( $new_instance['ns_aw_custom_title'] ) : '';
+return $instance;
+}
+
+function widget($args, $instance) {
+if(is_single()){
+global $wpdb;
+extract($args);
+echo $before_widget;
+$ns_aw_custom_title = apply_filters( 'widget_title', $instance['ns_aw_custom_title'] );
+if ( !empty( $name ) ) { echo $before_title . $ns_aw_custom_title .
+$after_title; }
+$nsaw_author = get_the_author_meta( 'ID' );
+$nsaw_author_email = get_the_author_meta('user_email',$nsaw_author);
+$nsaw_author_login_id = get_the_author_meta('user_login',$nsaw_author);
+$nsaw_author_nickname = get_the_author_meta('nickname',$nsaw_author); 
+$nsaw_author_display = get_the_author_meta('display_name',$nsaw_author);
+
+$nsaw_fb=get_the_author_meta( 'nsaw_fb', $nsaw_author );
+$nsaw_twitter=get_the_author_meta( 'nsaw_twitter', $nsaw_author );
+$nsaw_gp=get_the_author_meta( 'nsaw_gp', $nsaw_author );
+$nsaw_yt=get_the_author_meta( 'nsaw_yt', $nsaw_author );
+$nsaw_li=get_the_author_meta( 'nsaw_li', $nsaw_author );
+?>
+
+<div class="nsaw-wrap">
+<div class="nsaw-inner-wrap-name">
+<b><?php echo strtoupper($nsaw_author_display); ?></b>
+</div>
+<div class="nsaw-inner-wrap">
+<div class="nsaw-image">
+<?php
+echo get_avatar( $nsaw_author_email, '90','',$nsaw_author_login_id); 
+?>
+</div>
+<div class="nsaw-post">
+<div class="author-desc">
+<p>
+<?php the_author_meta('description'); ?><br>
+<?php if ( get_the_author_meta( 'url' ) ) { ?>
+	<a target="_blank" href="<?php the_author_meta( 'url' ) ?>" title="Author's Website"><img src="<?php  echo plugins_url('image\link.png',__FILE__)?>" /></a>
+<?php } ?>
+</p>
+</div>
+<p><?php echo number_format_i18n( get_the_author_posts() ); ?> Posts </br>
+<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+	<?php printf( __( 'View all posts by %s', 'nsaw' ), get_the_author() ); ?>
+</a></p>
+</div>
+
+<div class="nsaw-social">
+<?php if(!empty($nsaw_fb)) {?>
+<a target="_blank" href="<?php echo get_the_author_meta( 'nsaw_fb', $nsaw_author ); ?>"><img src="<?php  echo plugins_url('image\facebook.png',__FILE__)?>" /></a>
+<?php } ?>
+
+<?php if(!empty($nsaw_twitter)) {?>
+<a target="_blank" href="<?php echo get_the_author_meta( 'nsaw_twitter', $nsaw_author ); ?>"><img src="<?php  echo plugins_url('image\twitter.png',__FILE__)?>" /></a>
+<?php } ?>
+
+<?php if(!empty($nsaw_gp)) {?>
+<a target="_blank" href="<?php echo get_the_author_meta( 'nsaw_gp', $nsaw_author ); ?>"><img src="<?php  echo plugins_url('image\googleplus.png',__FILE__)?>" /></a>
+<?php } ?>
+
+<?php if(!empty($nsaw_yt)) {?>
+<a target="_blank" href="<?php echo get_the_author_meta( 'nsaw_yt', $nsaw_author ); ?>"><img src="<?php  echo plugins_url('image\youtube.png',__FILE__)?>" /></a>
+<?php } ?>
+
+<?php if(!empty($nsaw_li)) {?>
+<a target="_blank" href="<?php echo get_the_author_meta( 'nsaw_li', $nsaw_author ); ?>"><img src="<?php  echo plugins_url('image\linkedin.png',__FILE__)?>" /></a>
+</div>
+<?php } ?>
+
+</div>
+</div>
+<div class="clear"></div>
+
+<?php
+echo $after_widget;
+}
+}
+}
+
+add_action( 'show_user_profile', 'nsaw_show_profile' );
+add_action( 'edit_user_profile', 'nsaw_show_profile' );
+
+function nsaw_show_profile( $user ) { ?>
+
+	<h3>Social Profiles:</h3>
+	<table class="form-table">
+	<tr>
+	<th><label for="nsaw_fb_label">Facebook:</label></th>
+	<td>
+	<span class="description">Enter your FB Account URL:</span><br>
+	<input name="nsaw_fb" class="regular-text" type="url" placeholder="http://facebook.com/NetscripterInfo" value="<?php echo esc_attr( get_the_author_meta( 'nsaw_fb', $user->ID ) ); ?>" /><br>
+    <p style="color:#999; font-size: 12px;">leave it blank if you don't want to show this profile.</p>
+	</td>
+	</tr>
+
+	<tr>
+	<th><label for="nsaw_twitter_label">Twitter:</label></th>
+	<td>
+	<span class="description">Enter your Twitter Account:</span><br>
+	<input name="nsaw_twitter" class="regular-text" type="url" placeholder="http://twitter.com/NetscripterInfo" value="<?php echo esc_attr( get_the_author_meta( 'nsaw_twitter', $user->ID ) ); ?>" /><br>
+	<p style="color:#999; font-size: 12px;">leave it blank if you don't want to show this profile.</p>
+	</td>
+	</tr>
+
+	<tr>
+	<th><label for="nsaw_GP_label">Google Plus:</label></th>
+	<td>
+	<span class="description">Enter your Google Plus Account URL:</span><br>
+	<input name="nsaw_gp" class="regular-text" type="url" placeholder="https://plus.google.com/+NetscripterMe-WP" value="<?php echo esc_attr( get_the_author_meta( 'nsaw_gp', $user->ID ) ); ?>" /><br>
+	<p style="color:#999; font-size: 12px;">leave it blank if you don't want to show this profile.</p>
+	</td>
+	</tr>
+
+	<tr>
+	<th><label for="nsaw_yt_label">YouTube:</label></th>
+	<td>
+	<span class="description">Enter your YouTube Channel URL:</span><br>
+	<input name="nsaw_yt" class="regular-text" type="url" placeholder="https://www.youtube.com/user/netscripter" value="<?php echo esc_attr( get_the_author_meta( 'nsaw_yt', $user->ID ) ); ?>" /><br>
+	<p style="color:#999; font-size: 12px;">leave it blank if you don't want to show this profile.</p>
+	</td>
+	</tr>
+
+	<tr>
+	<th><label for="nsaw_LI_label">Linked In:</label></th>
+	<td>
+	<span class="description">Enter your Linked In Account:</span><br>
+	<input name="nsaw_li" class="regular-text" type="url" placeholder="http://in.linkedin.com/in/misharnet/" value="<?php echo esc_attr( get_the_author_meta( 'nsaw_li', $user->ID ) ); ?>" /><br>
+	<p style="color:#999; font-size: 12px;">leave it blank if you don't want to show this profile.</p>
+	</td>
+	</tr>
+    </table>
+
+<?php
+}
+add_action( 'personal_options_update', 'nsaw_edit_save_profile' );
+add_action( 'edit_user_profile_update', 'nsaw_edit_save_profile' );
+
+function nsaw_edit_save_profile( $user_id ) {
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+	update_user_meta($user_id, 'nsaw_fb', $_POST['nsaw_fb'] );
+	update_user_meta($user_id, 'nsaw_twitter', $_POST['nsaw_twitter'] );
+	update_user_meta($user_id, 'nsaw_gp', $_POST['nsaw_gp'] );
+	update_user_meta($user_id, 'nsaw_yt', $_POST['nsaw_yt'] );
+	update_user_meta($user_id, 'nsaw_li', $_POST['nsaw_li'] );	
+}
+
+?>
